f425d72b5913b38ce81746fa15e39ab1c6d1d2f6
galsivan <sivan.gal@gmail.com>
2025-08-01 14:39:11 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (82.78%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Recursion
         # def dfs(i):
         #     if i == 0:
         #         return nums[0]
@@ -8,15 +9,30 @@ class Solution:
         #     return max(nums[i] + dfs(i - 2), dfs(i - 1))
         # return dfs(len(nums) - 1)
 
-        res = [0] * len(nums)
-        res[0] = nums[0]
-        if len(res) > 1:
-            res[1] = max(nums[0], nums[1])
+        # dynamic programming (bottoms up)
+        # res = [0] * len(nums)
+        # res[0] = nums[0]
+        # if len(res) > 1:
+        #     res[1] = max(nums[0], nums[1])
+
+        # for i in range(2, len(nums)):
+        #     res[i] = max(nums[i] + res[i - 2], res[i - 1])
+        
+        # return res[-1]
+
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        
+        prev = nums[0]
+        cur = max(prev, nums[1])
 
         for i in range(2, len(nums)):
-            res[i] = max(nums[i] + res[i - 2], res[i - 1])
+            temp = cur
+            cur = max(nums[i] + prev, cur)
+            prev = temp
         
-        return res[-1]
-
+        return cur
 
 
