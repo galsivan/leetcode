d262c204f1034ec25ac3abffcc94f6f83f09b4e1
galsivan <sivan.gal@gmail.com>
2025-08-04 15:13:10 -0400

    Time: 749 ms (61.55%) | Memory: 18.1 MB (87.84%) - LeetSync

---
+++ 322-coin-change/coin-change.py
@@ -0,0 +1,15 @@
+class Solution:
+    # the min number of coins it takes to get to a certain amount
+    # dp[0] = 0
+    # dp[1] = 1
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for i in range(1, amount + 1):
+            for c in coins:
+                if i - c >= 0:
+                    dp[i] = min(dp[i], 1 + dp[i - c])
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1
+        
