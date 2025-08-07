70ec78c9e63e68515a7346e94ea08140090a5cec
galsivan <sivan.gal@gmail.com>
2025-08-07 12:44:54 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (26.70%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -2,20 +2,13 @@ class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1 + str2 != str2 + str1:
             return ""
+        len1 = len(str1)
+        len2 = len(str2)
         
-        if len(str2) < len(str1):
-            shorter = str2
-            longer = str1
-        else:
-            shorter = str1
-            longer = str2
-
-        s = len(shorter)
-        l = len(longer)
-        for i in range(l,0,-1):
-            if l % i == 0 and s % i == 0:
-                if shorter[:i] *  (s // i) == shorter and \
-                    shorter[:i] * (l // i) == longer:
-                        return shorter[:i]
+        for i in range(min(len1, len2),0,-1):
+            if len1 % i == 0 and len2 % i == 0:
+                if str1[:i] *  (len1 // i) == str1 and \
+                    str1[:i] * (len2 // i) == str2:
+                        return str1[:i]
         return ""
         
